41e341e3f8e4ff7b3249c4b74656936452c81e86
ishanpandey332 <ishanpandey332@gmail.com>
2026-01-14 07:03:12 +0000

    Sync LeetCode submission Runtime - 79 ms (2.04%), Memory - 28.4 MB (5.01%)

--- solutions/0344-reverse-string/solution.cpp
+++ solutions/0344-reverse-string/solution.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        int st=0, end=s.size()-1;
-        while(st < end){
-            swap(s[st++], s[end--]);
+    void reverseString(vector<char>& s,int i=0) {
+        int n= s.size();
+        if ( i >= n/2){
+            return;
         }
+        swap(s[i], s[n - i - 1]);
+        reverseString(s, i+1);
+        cout<< s[i]<< endl;
+
         
     }
 };
