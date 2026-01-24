52126885c5926a3e5d120fff3394908e20cee251
ishanpandey332 <ishanpandey332@gmail.com>
2026-01-24 06:20:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (42.65%)

--- solutions/0050-powx-n/solution.cpp
+++ solutions/0050-powx-n/solution.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        long long N=n;
+        if(n<0){
+            x=1/x;
+            N=-N;
 
-        if(n==0) return 1.0;
-        if(x==0) return 0.0;
-        if(x==1) return 1.0;
-        if(x==-1 && n%2==0) return 1;
-        if(x==-1 && n%2!= 0) return -1;
-       long binform = n;
-       if(n<0){
-        x= 1/x;
-        binform= -binform;
-       }
-       double ans=1;
-
-       while(binform>0){
-        if(binform % 2==1){
-            ans*=x;
         }
-        x*=x;
-        binform /= 2;
-
-               }
-               return ans;
+        return fastPow(x,N);
+        
+    }
+    private:
+    double fastPow(double x, long long n){
+        if(n==0){
+            return 1;
+        }
+        double half= fastPow(x,n/2);
+        if(n%2==0){
+            return half*half;
+        }
+        else{
+            return half*half*x;
+        }
     }
 };
