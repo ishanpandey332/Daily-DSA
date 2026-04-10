b77589f7694778ff24a2cf5a3b57bd6bd84f5856
ishanpandey332 <ishanpandey332@gmail.com>
2026-04-10 19:02:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 29.8 MB (37.98%)

---
+++ solutions/4115-minimum-distance-between-three-equal-elements-i/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+         unordered_map<int, vector<int>> positions;
+    
+    for (int i = 0; i < nums.size(); i++) {
+        positions[nums[i]].push_back(i);
+    }
+    
+    int ans = INT_MAX;
+    
+    for (auto &p : positions) {
+        auto &idx = p.second;
+        if (idx.size() < 3) continue;
+        
+        for (int i = 0; i + 2 < idx.size(); i++) {
+            int dist = 2 * (idx[i+2] - idx[i]);
+            ans = min(ans, dist);
+        }
+    }
+    
+    return ans == INT_MAX ? -1 : ans;
+
+        
+    }
+};
